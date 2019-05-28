77fceaecfd97f57d8d8ec12982dc9d754fd8a557
Ritesh Kr <ritesh.kumar@toppr.com>
2019-05-28 11:56:29 +0530

    decreases sku_id and price size for postgresql

--- db/migrate/20190526131842_create_products.rb
+++ db/migrate/20190526131842_create_products.rb
@@ -2,10 +2,10 @@ class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
       t.string :name
-      t.decimal :price, precision: 10, scale: 2
+      t.decimal :price, precision: 5, scale: 2
       t.date :expire_date
       t.text :images
-      t.integer :sku_id, limit: 12
+      t.integer :sku_id
 
       t.timestamps
     end
