4d6d5eae9d0c20a3dc7e39da8d43b06389139bda
Ritesh Kr <ritesh.kumar@toppr.com>
2019-05-28 11:29:20 +0530

    configs changed for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root 'welcome#index'
+  
   get 'admin/products', to: "products#index", as: :products
 
   get 'admin/products/:id', to: "products#show", as: :product
